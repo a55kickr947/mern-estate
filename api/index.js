a0f2e0b3e3967f1650f76e514ea8b6b0bec2df63
a55kickr947 <sumanths947@gmail.com>
2023-11-30 15:34:18 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+     console.log("connected to mongo")
+}).catch((err) => {
+     console.log(err)
+});
 
 app.listen(3000,() => {
      console.log("server is running");
