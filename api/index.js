8258d7ca6e2a1bd2e9fc94d5c82a6d559aa9fd16
a55kickr947 <sumanths947@gmail.com>
2023-12-01 15:17:32 +0530

    signup routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js'
-
+import authRouter from './routes/auth.routes.js'
 
 dotenv.config();
 const app = express();
@@ -13,8 +13,12 @@ mongoose.connect(process.env.MONGO).then(() => {
      console.log(err)
 });
 
+app.use(express.json());
+
+
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter)
+
 app.listen(3000,() => {
      console.log("server is running");
 });
-
-app.use("/api/user",userRouter);
